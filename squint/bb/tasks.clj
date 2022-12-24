d740a63f774d7308452b4c5b121a06f721b2684e
Michiel Borkent <michielborkent@gmail.com>
2022-12-24 20:54:17 +0100

    headers

--- squint/bb/tasks.clj
+++ squint/bb/tasks.clj
@@ -87,19 +87,19 @@
     (spit "deps.edn" deps)))
 
 (defn pull-request [{:keys [github-token branch]}]
-  (let [headers {:headers {"Accept" "application/vnd.github+json"
-                           "Authorization" (str "Bearer " github-token)
-                           "X-GitHub-Api-Version" "2022-11-28"}}
+  (let [headers {"Accept" "application/vnd.github+json"
+                 "Authorization" (str "Bearer " github-token)
+                 "X-GitHub-Api-Version" "2022-11-28"}
         resp (curl/post "https://api.github.com/repos/squint-cljs/squint/pulls"
-                        headers
-                        :body (json/generate-string {:title "Bump common"
-                                                     :body "Bump common"
-                                                     :head branch
-                                                     :base "main"}))
+                        {:headers headers
+                         :body (json/generate-string {:title "Bump common"
+                                                      :body "Bump common"
+                                                      :head branch
+                                                      :base "main"})})
         body (:body resp)
         body (json/parse-string body true)
         url (:url body)
         comment-url (str url "/comments")]
     (curl/post comment-url
-               headers
-               :body (json/generate-string {:body "@borkdude: please merge!"}))))
+               {:headers headers
+                :body (json/generate-string {:body "@borkdude: please merge!"})})))
